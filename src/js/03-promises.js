2e34856163d6d4d2714c9becfa2d46298a520e34
Viktoriia181818 <vcibulskaa4@gmail.com>
2022-12-30 11:30:32 +0200

    update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,44 +1,39 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const formElem = document.querySelector('.form');
-formElem.addEventListener('submit', onSubmitBtn);
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onSubmitForm);
 
-function onSubmitBtn(e) {
+function onSubmitForm(e) {
   e.preventDefault();
-}
 
-let delay = Number(e.target.delay.value);
-let step = Number(e.target.step.value);
-let amount = Number(e.target.amount.value);
+  let delay = Number(e.currentTarget.delay.value);
+  const step = Number(e.currentTarget.step.value);
+  const amount = Number(e.currentTarget.amount.value);
 
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  const promise = { position, delay };
-
-  return new Promise((resolve, reject) => {
-    if (shouldResolve) {
-      resolve(promise);
-    }
-    reject(promise);
-  });
-}
-
-for (let position = 1; position <= amount; position += 1) {
-  createPromise(2, 1500)
-    .then(({ position, delay }) => {
-      setTimeout(() => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, { useIcon: false });
-      }, delay);
-    })
-   .catch(({ position, delay }) => {
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, { useIcon: false });
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
         setTimeout(() => {
           Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, { useIcon: false });
         }, delay);
       });
     delay += step;
   }
-  
+}
 
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  const objectPromise = { position, delay };
 
-  
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(objectPromise);
+    }
+    reject(objectPromise);
+  });
+}
